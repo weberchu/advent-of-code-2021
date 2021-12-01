447dbf315a810da773952f49ce1f4d8c11e59c1c
Weber Chu <weberchu@amazon.com>
2021-12-01 15:40:56 +0000

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,33 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var numberOfIncrement = 0
+        for (i in 1 until input.size) {
+            if (input[i].toInt() > input[i-1].toInt()) {
+                numberOfIncrement++
+            }
+        }
+        return numberOfIncrement
     }
 
+    /**
+     * Compare the sliding window of size 3.
+     * Consider the first window comparison with the first 4 numbers - A, B, C, D
+     * The isIncrement boolean is B+C+D > A+B+C, which is effectively D > A.
+     * Therefore we can simplify compare input[i] > input[i-3]
+     *
+     */
     fun part2(input: List<String>): Int {
-        return input.size
+        var numberOfIncrement = 0
+        for (i in 3 until input.size) {
+            if (input[i].toInt() > input[i-3].toInt()) {
+                numberOfIncrement++
+            }
+        }
+        return numberOfIncrement
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+
+    println("Part 1: " + part1(input))
+    println("Part 2: " + part2(input))
 }
